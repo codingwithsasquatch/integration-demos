a9ec29956fdb9d1b79d5b99b6416aa22b2b781af
David Barkol <dbarkol@hotmail.com>
2018-08-15 14:57:45 -0600

    Added Product model and empty function app project

---
+++ Demos/NinjaStore/NinjaStore.Tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NinjaStore.Tests
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //TODO: Implement
+
+            Console.WriteLine("Press <enter> to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Demos/Products/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Products.Models
+{
+    public class Product
+    {
+        #region Properties
+
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public int Count { get; set; }
+
+        public decimal Price { get; set; }
+
+        #endregion
+    }
+}
