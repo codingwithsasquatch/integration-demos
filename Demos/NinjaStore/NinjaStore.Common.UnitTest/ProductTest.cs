d553b9fe6710fe61503040757e78c379f78b4619
David Barkol <dbarkol@hotmail.com>
2018-08-20 18:14:15 -0700

    Added unit test project for common.

---
+++ Demos/NinjaStore/NinjaStore.Common.UnitTest/ProductTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NinjaStore.Common.Models;
+using NinjaStore.Common.Repositories;
+
+namespace NinjaStore.Common.UnitTest
+{
+    [TestClass]
+    public class ProductTest
+    {
+        #region Data Members
+
+        private ProductRepository _productRepository;
+
+        #endregion
+
+        #region Initialize and Cleanup Methods
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            InitializeProductRepository().GetAwaiter().GetResult();
+        }
+
+        #endregion
+
+        #region Tests
+
+        [TestMethod]
+        public void GetAllProductsTest()
+        {
+            var result = _productRepository.GetAllProducts();
+            Assert.IsTrue(result.Count > 0);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private async Task InitializeProductRepository()
+        {
+            var docDbSettings = new DocumentDbSettings
+            {
+                AuthKey = "",
+                CollectionId = "",
+                DatabaseId = "",
+                Endpoint = ""
+            };
+
+            _productRepository = new ProductRepository(docDbSettings);
+            await _productRepository.Initialize();
+        }
+
+        #endregion
+    }
+}
+
+
